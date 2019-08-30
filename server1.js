7bf1fd80f367ef935aba3a7ecdccc9400e678872
AKINOLA Victor <akinolavictor26@gmail.com>
2019-08-30 07:51:08 +0100

    Update server1.js

--- server1.js
+++ server1.js
@@ -5,7 +5,7 @@ let http = require('http');
 let server = http.createServer(function(req, res){
     console.log('request was made: '+ req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey n');
+    res.end('Hello');
 });
 
 server.listen(3000, '127.0.0.1');
