040f3a7c5d629b5bff1884711250d72c0abf67c8
AKINOLA Victor <akinolavictor26@gmail.com>
2019-08-30 07:50:40 +0100

    Update server1.js

--- server1.js
+++ server1.js
@@ -1,10 +1,11 @@
 /*running a server on the web*/
 
+//using the require module
 let http = require('http');
 let server = http.createServer(function(req, res){
     console.log('request was made: '+ req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey ninjas');
+    res.end('Hey n');
 });
 
 server.listen(3000, '127.0.0.1');
