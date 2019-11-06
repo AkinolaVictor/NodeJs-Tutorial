424bc828f02737593bc18a6ce8aac26fd62d463a
AKINOLA Victor <akinolavictor26@gmail.com>
2019-11-06 08:21:20 +0100

    Update app2.js

--- notes-app-by-andrew-mead/app2.js
+++ notes-app-by-andrew-mead/app2.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
+//this is useful 
 //fs.writeFileSync('note.txt', 'This is a fresh start to learning nodejs,' )
 fs.appendFileSync('note.txt', 'id be a PRO soon, as long as God is on my side')
